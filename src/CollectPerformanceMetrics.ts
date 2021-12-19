3c275e83f7f293ca39e8a4b1dd89937a7b52f16a
Julien <julien@ripouteau.com>
2021-12-19 04:07:41 +0100

    ✨ added includeRouteParams and includeQueryParams, close #1

--- src/CollectPerformanceMetrics.ts
+++ src/CollectPerformanceMetrics.ts
@@ -4,18 +4,30 @@ import { Metrics } from './Metrics'
 export class CollectPerformanceMetrics {
   constructor(protected metrics: Metrics, protected config: any) {}
 
-  public async handle({ request, response }: HttpContextContract, next: () => Promise<void>) {
-    const { enabled: enableHttpMetric } = this.config.httpMetric
+  public async handle(
+    { request, response, route }: HttpContextContract,
+    next: () => Promise<void>
+  ) {
+    const httpMetricOptions = this.config.httpMetric
     const enableThroughputMetric = this.config.throughputMetric.enabled
 
     /**
      * Start HTTP request timer.
      */
     let stopHttpRequestTimer
-    if (enableHttpMetric) {
+    if (httpMetricOptions.enableHttpMetric) {
+      const includeRouteParams = httpMetricOptions.includeRouteParams
+      const includeQueryParams = httpMetricOptions.includeQueryParams
+
+      let url = includeRouteParams ? route?.pattern : request.url()
+
+      if (includeQueryParams) {
+        url += `?${request.parsedUrl.query}`
+      }
+
       stopHttpRequestTimer = this.metrics.httpMetric.startTimer({
         method: request.method(),
-        url: request.url(),
+        url,
       })
     }
 
