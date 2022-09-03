5111cc1a461bb9c67eba2992881f86bd1550324d
Julien <julien@ripouteau.com>
2022-09-03 23:58:56 +0200

    refactor: some refactor + add tests

--- providers/PrometheusProvider.ts
+++ providers/PrometheusProvider.ts
@@ -1,6 +1,7 @@
 import * as prometheus from 'prom-client'
+import { Metrics } from '../src/Metrics'
+import type { PrometheusConfig } from '@ioc:Adonis/Prometheus'
 import type { ApplicationContract, IocContract } from '@ioc:Adonis/Core/Application'
-import type { ConfigContract } from '@ioc:Adonis/Core/Config'
 
 export default class PrometheusProvider {
   public static needsApplication = true
@@ -13,13 +14,13 @@ export default class PrometheusProvider {
   /**
    * Expose metrics on the given endpoint
    */
-  private exposeMetricsEndpoint(Config: ConfigContract) {
-    if (Config.get('prometheus.exposeHttpEndpoint') === false) {
+  private exposeMetricsEndpoint(config: PrometheusConfig) {
+    if (config.exposeHttpEndpoint === false) {
       return
     }
 
     const router = this.app.container.resolveBinding('Adonis/Core/Route')
-    const urlPath = Config.get('prometheus.endpoint') || '/metrics'
+    const urlPath = config.endpoint || '/metrics'
 
     router.get(urlPath, async ({ response }) => {
       response
@@ -31,32 +32,33 @@ export default class PrometheusProvider {
   /**
    * Collect system metrics if enabled
    */
-  private collectSystemMetrics(Config: ConfigContract) {
-    const systemMetrics = Config.get('prometheus.systemMetrics')
-    if (systemMetrics.enabled) {
-      prometheus.collectDefaultMetrics(systemMetrics)
+  private collectSystemMetrics(config: PrometheusConfig) {
+    if (config.systemMetrics.enabled) {
+      prometheus.collectDefaultMetrics(config.systemMetrics)
     }
   }
 
   public register(): void {
-    const Config = this.app.container.resolveBinding('Adonis/Core/Config')
+    const promConfig: PrometheusConfig = this.app.container
+      .resolveBinding('Adonis/Core/Config')
+      .get('prometheus')
 
-    this.collectSystemMetrics(Config)
-    this.exposeMetricsEndpoint(Config)
+    this.collectSystemMetrics(promConfig)
+    this.exposeMetricsEndpoint(promConfig)
+
+    const metrics = new Metrics(promConfig)
+    if (promConfig.uptimeMetric.enabled) {
+      metrics.uptimeMetric.inc(1)
+    }
 
     this.app.container.singleton('Adonis/Prometheus', () => prometheus)
     this.app.container.singleton('Adonis/Prometheus/Middlewares/CollectPerformanceMetrics', () => {
       const { CollectPerformanceMetrics } = require('../src/CollectPerformanceMetrics')
-      const { Metrics } = require('../src/Metrics')
-      const config = this.container.use('Adonis/Core/Config').get('prometheus')
-
-      const metrics = new Metrics(config)
-      const enableUptimeMetric = config.uptimeMetric.enabled
-      if (enableUptimeMetric) {
-        metrics.uptimeMetric.inc(1)
-      }
-
-      return new CollectPerformanceMetrics(metrics, config)
+      return new CollectPerformanceMetrics(metrics, promConfig)
     })
+  }
+
+  public async shutdown() {
+    prometheus.register.clear()
   }
 }
