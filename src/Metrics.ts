79c6661854c381628cd0b3bcab40484f819444fe
Julien <julien@ripouteau.com>
2023-05-01 21:55:15 +0200

    chore: update jsdoc

--- src/Metrics.ts
+++ src/Metrics.ts
@@ -1,24 +1,26 @@
 import * as prometheus from 'prom-client'
 
 export class Metrics {
+  /**
+   * Total time each HTTP request takes.
+   */
   public httpMetric: prometheus.Histogram<string>
+
+  /**
+   * Uptime performance of the application.
+   */
   public uptimeMetric: prometheus.Gauge<string>
+
+  /**
+   * No. of request handled.
+   */
   public throughputMetric: prometheus.Counter<string>
 
   constructor(protected config: any) {
-    /**
-     * Total time each HTTP request takes.
-     */
     this.httpMetric = new prometheus.Histogram(config.httpMetric)
 
-    /**
-     * Uptime performance of the application.
-     */
     this.uptimeMetric = new prometheus.Gauge(config.uptimeMetric)
 
-    /**
-     * No. of request handled.
-     */
     this.throughputMetric = new prometheus.Counter(config.throughputMetric)
   }
 }
